910eef5cad622ef5b8ba9e8073c1f2a62d76ef36
Riyad Firdaus <riyadf27@gmail.com>
2022-10-22 21:01:34 +0700

    Main Drawing Function Completed

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-empty */
 const colorPalette = [
-     '#1abc9c', '#2ecc71',
-     '#3498db', '#9b59b6',
-     '#34495e', '#16a085',
-     '#27ae60', '#2980b9',
-     '#8e44ad', '#2c3e50',
-     '#f1c40f', '#e67e22',
-     '#e74c3c', '#ecf0f1',
-     '#95a5a6', '#f39c12',
-     '#d35400', '#c0392b',
-     '#bdc3c7', '#7f8c8d'
+    '#1abc9c', '#2ecc71',
+    '#3498db', '#9b59b6',
+    '#34495e', '#16a085',
+    '#27ae60', '#2980b9',
+    '#8e44ad', '#2c3e50',
+    '#f1c40f', '#e67e22',
+    '#e74c3c', '#ecf0f1',
+    '#95a5a6', '#f39c12',
+    '#d35400', '#c0392b',
+    '#bdc3c7', '#7f8c8d'
 ];
 
 const canvas = document.getElementById('canvasGrid');
@@ -19,12 +19,17 @@ const smallbtn = document.getElementById('small');
 const mediumbtn = document.getElementById('medium');
 const largebtn = document.getElementById('large');
 let mouseColor = '#2c3e50';
+let mouseDown = false;
 
 function createCanvas(size = 32){
     canvas.innerHTML = '';
     canvas.style.gridTemplateColumns = `repeat(${size}, auto)`;
     for(let i=1; i<= size*size; i++){
         const pixel = document.createElement('div');
+        pixel.addEventListener('mousedown', ()=> mouseDown=true);
+        pixel.addEventListener('mouseup', ()=> mouseDown= false);
+        pixel.addEventListener('mousedown', changeColor);
+        pixel.addEventListener('mouseenter', changeColor);
         canvas.appendChild(pixel);
     }
 }
@@ -34,6 +39,7 @@ function createPalette(){
     for(let i=0; i< 4*5; i++){
         const pixel = document.createElement('div');
         pixel.style.backgroundColor = `${colorPalette[i]}`;
+        pixel.addEventListener('click', () => setMouseColor(colorPalette[i]));
         palette.appendChild(pixel);
     }
 }
@@ -49,25 +55,12 @@ function setMouseColor(color){
     }
     document.getElementsByTagName('head')[0].appendChild(style); 
 }
-
-function changeColor(){
-
+function changeColor(e){
+    console.log(mouseDown);
+    if (e.type === 'mouseenter' && !mouseDown)
+        return;
+    e.target.style.backgroundColor = mouseColor;
 }
-
-let allPixel = canvas.childNodes;
-allPixel.forEach(pixel => {
-    pixel.addEventListener('click', () => changeColor);
-});
-
-let paletteColor = palette.childNodes;
-console.log(paletteColor);
-paletteColor.forEach(color => {
-    color.addEventListener('click', () =>{
-        setMouseColor(color.style.backgroundColor);
-        console.log('test');
-        this.stopPropagation();
-    });
-});
 
 createCanvas();
 createPalette();
