99efa1488634cee570c5c6627927583787b7dd1f
Riyad Firdaus <riyadf27@gmail.com>
2022-10-23 19:03:21 +0700

    add color mode

--- main.js
+++ main.js
@@ -13,14 +13,26 @@ const colorPalette = [
     '#bdc3c7', '#7f8c8d'
 ];
 
+const rainbowPalette = [
+    '#e74c3c', '#e67e22' , '#f1c40f',
+    '#2ecc71', '#1abc9c',
+    '#3498db', '#9b59b6'
+];
+
 const canvas = document.getElementById('canvasGrid');
 const palette = document.getElementById('colorPalette');
 const smallbtn = document.getElementById('small');
 const mediumbtn = document.getElementById('medium');
 const largebtn = document.getElementById('large');
 const clearbtn = document.getElementById('clear');
+const rainbowModeBtn= document.getElementById('rainbowMode');
+const randomModeBtn= document.getElementById('randomMode');
+const eraserBtn= document.getElementById('eraser');
+
 let mouseColor = '#2c3e50';
+let rainbowCounter = 0;
 let mouseDown = false;
+let currentMode = 'default';
 let size;
 
 function createCanvas(value = 32){
@@ -29,10 +41,8 @@ function createCanvas(value = 32){
     canvas.style.gridTemplateColumns = `repeat(${size}, auto)`;
     for(let i=1; i<= size*size; i++){
         const pixel = document.createElement('div');
-        pixel.addEventListener('mousedown', ()=> mouseDown=true);
-        pixel.addEventListener('mouseup', ()=> mouseDown= false);
-        pixel.addEventListener('mousedown', changeColor);
-        pixel.addEventListener('mouseenter', changeColor);
+        pixel.addEventListener('mousedown', drawColor);
+        pixel.addEventListener('mouseenter', drawColor);
         canvas.appendChild(pixel);
     }
 }
@@ -42,15 +52,22 @@ function createPalette(){
     for(let i=0; i< 4*5; i++){
         const pixel = document.createElement('div');
         pixel.style.backgroundColor = `${colorPalette[i]}`;
-        pixel.addEventListener('click', () => setMouseColor(colorPalette[i]));
+        pixel.addEventListener('click', () => {
+            setMouseColor(colorPalette[i]);
+            currentMode ='default';
+        });
         palette.appendChild(pixel);
     }
 }
 
 function setMouseColor(color){
     mouseColor = color;
+    setHoverColor(color);
+}
+
+function setHoverColor(color){
     let style = document.createElement('style');
-    let hover = `#canvasGrid div:hover { background-color: ${mouseColor} }`;  
+    let hover = `#canvasGrid div:hover { background-color: ${mouseColor} !important}`;  
     if (style.styleSheet) {
         style.styleSheet.cssText = hover;
     } else {
@@ -58,20 +75,52 @@ function setMouseColor(color){
     }
     document.getElementsByTagName('head')[0].appendChild(style); 
 }
-function changeColor(e){
-    if (e.type === 'mouseenter' && !mouseDown)
-        return;
-    e.target.style.backgroundColor = mouseColor;
+
+function drawColor(e){
+    if (e.type === 'mouseenter' && !mouseDown) return;
+    if(currentMode == 'random'){
+        e.target.style.backgroundColor = mouseColor;
+        setMouseColor(getRandomColor());
+    }
+    else if(currentMode == 'rainbow'){
+        e.target.style.backgroundColor = mouseColor;
+        rainbowCounter = ++rainbowCounter%7;
+        setMouseColor(rainbowPalette[rainbowCounter]);
+    }
+    else if(currentMode == 'eraser'){
+        e.target.style.backgroundColor = mouseColor;
+        console.log(mouseColor);
+    }
+    else e.target.style.backgroundColor = mouseColor;
 }
 
-smallbtn.addEventListener('click', ()=> createCanvas(16));
-mediumbtn.addEventListener('click', ()=> createCanvas(32));
-largebtn.addEventListener('click', ()=> createCanvas(64));
-clearbtn.addEventListener('click',()=> createCanvas(size))
+function getRandomColor(){
+    let i = Math.floor(Math.random()*20);
+    return colorPalette[i];
+}
+
+smallbtn.addEventListener('click', () => createCanvas(16));
+mediumbtn.addEventListener('click', () => createCanvas(32));
+largebtn.addEventListener('click', () => createCanvas(64));
+clearbtn.addEventListener('click',() => createCanvas(size));
+eraserBtn.addEventListener('click', () =>{
+    currentMode = 'eraser';
+    setMouseColor('#ffffff');
+});
+rainbowModeBtn.addEventListener('click', () =>{
+    currentMode = 'rainbow';
+    rainbowCounter = 0;
+    setMouseColor(rainbowPalette[0]);
+});
+randomModeBtn.addEventListener('click', () =>{
+    currentMode = 'random';
+    setMouseColor(getRandomColor);
+});
 
 window.onload = () =>{
     createCanvas();
     createPalette();
     setMouseColor(mouseColor);
 }
-;
+window.addEventListener('mousedown', () => mouseDown = true);
+window.addEventListener('mouseup', () => mouseDown = false);
